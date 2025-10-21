1d391832461cf02c9fc97be62ef8bcf4328a4433
ternopolskaiasa <serafimaternopolskaya@gmail.com>
2025-10-21 17:02:18 +0300

    Update UserRequestDTO.java

--- 2homework_4/src/main/java/org/example/dto/UserRequestDTO.java
+++ 2homework_4/src/main/java/org/example/dto/UserRequestDTO.java
@@ -7,14 +7,28 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+/**
+ * Data transfer object (DTO) used for receiving requests related to user creation or modification.
+ * Includes validation constraints for the incoming data.
+ */
 @Getter
 @Setter
 public class UserRequestDTO {
+
+    /**
+     * Full name of the user.
+     * Should have at least two characters and maximum fifty characters.
+     */
     @NotBlank
     @Size(min = 2, max = 50)
     private String name;
 
+    /**
+     * Email address of the user.
+     * Must conform to the correct email format.
+     */
     @NotBlank
     @Email(message = "Invalid email format")
     private String email;
 }
+
