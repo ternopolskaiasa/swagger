716ca84952783728722d55c4f86143f97bba359b
ternopolskaiasa <serafimaternopolskaya@gmail.com>
2025-10-21 16:07:20 +0300

    Update UserDAOImpl.java

--- 2homework_4/src/main/java/org/example/dao/UserDAOImpl.java
+++ 2homework_4/src/main/java/org/example/dao/UserDAOImpl.java
@@ -11,9 +11,17 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.util.List;
 
+/**
+ * Implementation of UserDAO interface utilizing Hibernate ORM for performing CRUD operations on User entities.
+ */
 public class UserDAOImpl implements UserDAO {
     private final SessionFactory sessionFactory;
 
+    /**
+     * Constructs a new instance of UserDAOImpl and initializes the Hibernate session factory.
+     * 
+     * @throws HibernateException if there's an issue while initializing Hibernate session factory.
+     */
     public UserDAOImpl() {
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure()
@@ -23,10 +31,17 @@ public class UserDAOImpl implements UserDAO {
                     .buildMetadata().buildSessionFactory();
         } catch (Exception e) {
             StandardServiceRegistryBuilder.destroy(registry);
-            throw new HibernateException("Hibernate's initialization failed!");
+            throw new HibernateException("Hibernate's initialization failed!", e);
         }
     }
 
+    /**
+     * Retrieves a user by its id.
+     *
+     * @param id the unique identifier of the user to fetch.
+     * @return the User object associated with the given ID.
+     * @throws Exception if the user could not be found or another problem occurs.
+     */
     @Override
     public User getUserById(Long id) throws Exception {
         try (Session session = sessionFactory.openSession()) {
@@ -36,6 +51,12 @@ public class UserDAOImpl implements UserDAO {
         }
     }
 
+    /**
+     * Get all users from the db.
+     *
+     * @return a list of all User objects.
+     * @throws Exception if problems arise while fetching users from the db.
+     */
     @Override
     public List<User> getAllUsers() throws Exception {
         try (Session session = sessionFactory.openSession()) {
@@ -45,6 +66,12 @@ public class UserDAOImpl implements UserDAO {
         }
     }
 
+    /**
+     * Saves a new user to the db.
+     *
+     * @param user the User object to persist.
+     * @throws Exception if problems arise while saving the user.
+     */
     @Override
     public void saveUser(User user) throws Exception {
         try (Session session = sessionFactory.openSession()) {
@@ -56,6 +83,12 @@ public class UserDAOImpl implements UserDAO {
         }
     }
 
+    /**
+     * Updates an existing user in the db.
+     *
+     * @param user the User object with updated attributes.
+     * @throws Exception if the user couldn't be found or other problems occurred.
+     */
     @Override
     public void updateUser(User user) throws Exception {
         try (Session session = sessionFactory.openSession()) {
@@ -77,6 +110,12 @@ public class UserDAOImpl implements UserDAO {
         }
     }
 
+    /**
+     * Deletes a user from the db by its id.
+     *
+     * @param id the unique identifier of the user to delete.
+     * @throws Exception if the user couldn't be found or other problems occurred.
+     */
     @Override
     public void deleteUser(Long id) throws Exception {
         try (Session session = sessionFactory.openSession()) {
