a71966bcd4f9c380544785f641bf5a753b4c2f0a
ternopolskaiasa <serafimaternopolskaya@gmail.com>
2025-10-21 17:04:01 +0300

    Update UserResponseDTO.java

--- 2homework_4/src/main/java/org/example/dto/UserResponseDTO.java
+++ 2homework_4/src/main/java/org/example/dto/UserResponseDTO.java
@@ -3,10 +3,26 @@ package org.example.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Response Data Transfer Object (DTO) used for sending user information in API responses.
+ * Contains minimal but essential user details.
+ */
 @Getter
 @Setter
 public class UserResponseDTO {
+
+    /**
+     * Unique identifier of the user.
+     */
     private Long id;
+
+    /**
+     * Full name of the user.
+     */
     private String name;
+
+    /**
+     * Email address of the user.
+     */
     private String email;
 }
